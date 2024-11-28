76c661f565450dbb7c31bbbaacc515283e330c3e
Tadasu Isogawa <145603+tisogawa@users.noreply.github.com>
2024-11-28 11:15:12 +0900

    PHP 8.4

--- build.sh
+++ build.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
-wget https://github.com/box-project/box/releases/latest/download/box.phar -O ./box.phar
-wget https://github.com/composer/composer/releases/latest/download/composer.phar -O ./composer.phar
+curl -sLO https://github.com/box-project/box/releases/latest/download/box.phar
+curl -sLO https://github.com/composer/composer/releases/latest/download/composer.phar
 
-if [ ! -f ./composer.lock ]; then
-  php ./composer.phar install --no-dev
-else
+if [ -e ./composer.lock ]; then
   php ./composer.phar update --no-dev
+else
+  php ./composer.phar install --no-dev
 fi
 
-php ./box.phar compile && ./tzdate
+php -d error_reporting=24575 ./box.phar compile && ./tzdate
